ff1f8d15bbe51fade60c10841c7610a597584257
Jorge Medina <medinarosas.jorgealberto@gmail.com>
2023-08-24 14:01:54 -0600

    Added context aware, and added a few logger info to console in prod profile

---
+++ src/main/java/org/springboot/app/config/ApplicationContextProvider.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package org.springboot.app.config;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import lombok.Generated;
+
+/**
+ * @author jmedina
+ *
+ */
+@Component
+@Generated
+public class ApplicationContextProvider implements ApplicationContextAware {
+
+	private final Logger logger = LogManager.getLogger(ApplicationContextProvider.class);
+	
+	private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    	this.logger.info( "================> applicationContext LOADED!" );
+        this.applicationContext = applicationContext;
+    }
+
+    public ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+}
